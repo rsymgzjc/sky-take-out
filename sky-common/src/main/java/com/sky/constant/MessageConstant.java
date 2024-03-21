4d10ed94f06cec8a4bab5c0f9579f7da90e634b1
Orimiya <1402151939@qq.com>
2024-03-21 21:41:16 +0800

    0321 2140

--- sky-common/src/main/java/com/sky/constant/MessageConstant.java
+++ sky-common/src/main/java/com/sky/constant/MessageConstant.java
@@ -12,5 +12,7 @@ public class MessageConstant {
     public static final String UNKNOWN_ERROR="未知错误";
     public static final String CATEGORY_BE_RELATED_BY_SETMEAL = "当前分类关联了套餐,不能删除";
     public static final String CATEGORY_BE_RELATED_BY_DISH = "当前分类关联了菜品,不能删除";
-
+    public static final String UPLOAD_FAILED="文件上传失败";
+    public static final String DISH_ON_SALE="当前菜品处于起售中，不能删除";
+    public static final String DISH_BE_RELATED_BY_SETMEAL="当前菜品被套餐关联了，不能删除";
 }
