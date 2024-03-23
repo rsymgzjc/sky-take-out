da6f0ec03b84adbe4c64b0bd01b77c443fc59c6f
Orimiya <1402151939@qq.com>
2024-03-23 19:50:10 +0800

    0323 1950

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.SetmealDish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -13,4 +16,11 @@ public interface SetmealDishMapper {
      * @return
      */
     List<Long> getSetmealIdsByDishIds(List<Long>Dishids);
+
+    /**
+     * 向关联套餐表插入N条数据
+     * @param setmealDishes
+     */
+    @AutoFill(OperationType.INSERT)
+    void insertBatch(List<SetmealDish>setmealDishes);
 }
